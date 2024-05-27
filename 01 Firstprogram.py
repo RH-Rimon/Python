43429a11e04df1fc27509c70232c1a2ff26b02f6
RH Rimon <83422826+RH-Rimon@users.noreply.github.com>
2024-05-27 09:35:17 +0600

    Add files via upload

---
+++ 01 Firstprogram.py
@@ -0,0 +1,4 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World

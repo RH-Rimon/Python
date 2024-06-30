6dfd7b502d8cebaad18a460ccc50d9d5f63a14ef
RH Rimon <83422826+RH-Rimon@users.noreply.github.com>
2024-07-01 00:31:58 +0600

    Add files via upload

---
+++ 30 Using of Strip.py
@@ -0,0 +1,6 @@
+
+sentence ="How can a clam in a clean cream can?"
+
+result = sentence.strip("?")
+
+print(result)
---
+++ 31 Uses of Split and Join.py
@@ -0,0 +1,12 @@
+
+sentence = 'How can a clam in a clean cream can?'
+
+print(sentence)
+
+words = sentence.split(" ")
+
+print(words)
+
+result = '+'.join(words)
+
+print(result)
